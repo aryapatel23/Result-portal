a23bf692f2d1807a5abed732036bf403b3c81cb0
Arya Patel <aryapatel.cg@gmail.com>
2025-07-02 08:53:55 +0530

    Delete Backend directory

--- Backend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { uploadResult, exportResults } = require("../controllers/adminController");
-const { authenticate, authorize } = require("../middleware/authMiddleware");
-
-router.post("/upload", authenticate, authorize("admin"), uploadResult);
-router.get("/export", authenticate, authorize("admin"), exportResults);
-
-module.exports = router;
--- Backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getResult } = require("../controllers/userController");
-
-// Student views result using query: /view?grNumber=GR001&standard=10
-router.get("/view", getResult);
-
-module.exports = router;
