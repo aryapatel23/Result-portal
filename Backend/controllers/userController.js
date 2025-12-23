9995461d2ccd1571334dc3c848d210f0b769abc8
arya patel <aryaunjha@gmail.com>
2025-12-23 18:16:45 +0530

    add the get student by Gr function to fetch the student by the GR for result

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const Result = require("../models/Result");
+const User = require("../models/User");
 
 exports.getResult = async (req, res) => {
   const { grNumber, standard } = req.query;
@@ -17,5 +18,26 @@ exports.getResult = async (req, res) => {
     res.json(result);
   } catch (error) {
     res.status(500).json({ message: "Error retrieving result", error });
+  }
+};
+
+exports.getStudentByGR = async (req, res) => {
+  const { grNumber } = req.params;
+
+  if (!grNumber) {
+    return res.status(400).json({ message: "GR Number is required" });
+  }
+
+  try {
+    const student = await User.findOne({ grNumber, role: 'student' })
+      .select('name grNumber dateOfBirth standard parentContact');
+
+    if (!student) {
+      return res.status(404).json({ message: "Student not found with this GR Number" });
+    }
+
+    res.json({ student });
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving student", error });
   }
 };
