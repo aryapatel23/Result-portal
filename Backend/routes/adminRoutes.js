da8f984f6cf8903fae860d3e2a814f7ba0c0f424
arya patel <aryaunjha@gmail.com>
2025-12-23 18:25:19 +0530

    update the routes and add the result-activity route

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { uploadResult, exportResults } = require("../controllers/adminController");
+const { uploadResult, exportResults, getResultsActivity } = require("../controllers/adminController");
 const { authenticate, authorize } = require("../middleware/authMiddleware");
 
 router.post("/upload", authenticate, authorize("admin"), uploadResult);
 router.get("/export", authenticate, authorize("admin"), exportResults);
+router.get("/results-activity", authenticate, authorize("admin"), getResultsActivity);
 
 module.exports = router;
