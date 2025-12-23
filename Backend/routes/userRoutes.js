4e474267b0e8a5b18ef9bc41a8c0927922bbbcd6
arya patel <aryaunjha@gmail.com>
2025-12-23 18:43:51 +0530

    add the dynemic route for GR no.

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getResult } = require("../controllers/userController");
+const { getResult, getStudentByGR } = require("../controllers/userController");
 
 // Student views result using query: /view?grNumber=GR001&standard=10
 router.get("/view", getResult);
+
+// Get student info by GR Number
+router.get("/student/:grNumber", getStudentByGR);
 
 module.exports = router;
