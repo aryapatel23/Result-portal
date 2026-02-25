2f6a306145984f61da158de53587aec8b4fbf8a4
aryapatel23 <aryapatel.cg@gmail.com>
2026-02-25 10:24:38 +0530

    chore: update packages and build config
    - Add ThemeProvider to App.tsx
    - Remove nativewind, tailwindcss, react-native-reanimated (build fix)
    - Add @types/react-native-vector-icons
    - Update Android build configuration

--- ResultApp/App.tsx
+++ ResultApp/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthProvider } from './src/context/AuthContext';
+import { ThemeProvider } from './src/context/ThemeContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
 function App() {
   return (
     <SafeAreaProvider>
-      <AuthProvider>
-        <AppNavigator />
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <AppNavigator />
+        </AuthProvider>
+      </ThemeProvider>
     </SafeAreaProvider>
   );
 }
